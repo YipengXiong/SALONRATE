526020337206c5140868d4db9b15d38e0d75a505
SylvieY <2590260y@student.gla.ac.uk>
2022-03-13 13:36:49 +0000

    show avatar, optimize css, add icons

---
+++ static/js/star.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    // $("#star").eq(parameter).onclick(()=>{ })
+
+    $("#mac").click(function(){
+        $("#mac").toggle();
+        $(".cmf").toggle(100);
+    })
+
+    $("#cancel").click(function(){
+        $("#mac").toggle();
+        $(".cmf").toggle(100);
+    })
+});
+
+// $.ajax({
+//     type:'POST',
+//     url:'',
+//     data:{
+//         'scrfmiddlewaretoken':'',
+//     }
+// })
+
