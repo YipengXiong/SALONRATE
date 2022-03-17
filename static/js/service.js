97070b87e80c6324b5baf92e1b828107103519c5
Paul <yingheting@gmail.com>
2022-03-17 15:21:59 +0000

    service comment star commit

--- static/js/service.js
+++ static/js/service.js
@@ -1,21 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Click "Write a comment", show the comment form
-    $("#mac").click(function(){
+    $("#mac").click(function () {
         $("#mac").toggle();
         $(".cmf").toggle(100);
     })
     // Click "Cancel", hide the comment form 
-    $("#cancel").click(function(){
+    $("#cancel").click(function () {
         $("#mac").toggle();
         $(".cmf").toggle(100);
     })
     // Click a star, transfer the value to the "star" field in the comment form 
-    $(".star-rate > label").click(function(){
+    $(".star-rate > label").click(function () {
         var rate = $(this).val();
         console.log(rate)
         $(".star-rate > label").removeClass("rated")
         for (let i = 0; i < rate; i++) {
-            $(".star-rate > label.class"+rate).addClass("rated")
+            $(".star-rate > label.class" + rate).addClass("rated")
         }
         $("#id_star").val(rate);
     })
@@ -28,4 +28,13 @@ $(document).ready(function() {
 //         'scrfmiddlewaretoken':'',
 //     }
 // })
+$("div.star-rate label").on("click", function () {
+    let current_star = $(this).text()
+    // console.log(current_star)
+    $("div.star-rate label").removeClass("rated")
+    for (let i = 0; i < current_star; i++) {
+        $("div.star-rate label.star"+i).addClass("rated")
+    }
+    $("input#id_star").val(current_star)
+})
 
