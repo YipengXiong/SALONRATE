d8ddd0007ce56bb6445f0d1b0318ade644cb9602
Chong-Cui <2589587C@student.gla.ac.uk>
2022-03-18 23:49:54 +0000

    add upload profile picture and revise profile layout

--- salonrate/views.py
+++ salonrate/views.py
@@ -80,6 +80,13 @@ def user_logout(request):
 
 @login_required
 def user_profile(request):
+    if request.method == 'POST':
+        file = request.FILES['profile_picture']
+        if file:
+            profile = UserProfile.objects.filter(user=request.user)[0]
+            profile.avatar=file
+            profile.save()
+    
     user = request.user
     profile = UserProfile.objects.filter(user=user)[0]
     comments = Comment.objects.filter(username=user)
